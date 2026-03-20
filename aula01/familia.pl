aac8dfccfbf2bb6ca4f28651030e02a5a412fc73
Carlos Eduardo Pereira <contato.devcarlos@gmail.com>
2026-03-20 08:09:20 -0300

    Adicionando novos fatos e regras

--- aula01/familia.pl
+++ aula01/familia.pl
@@ -1,11 +1,18 @@
 % Fatos
-pai(joao, maria).
-pai(joao, pedro).
+pai(joao, lucas).
+pai(joao, joaquim).
+pai(joaquim, bruno).
+pai(pedro, silvia).
 
-% Regras 
-filho(X,Y) :- pai(Y,X).
-irmaos(X, Y, Z) :- pai(Z, X), pai(Z, Y), \+X = Y.
+mae(maria, lucas).
+mae(maria, joaquim).
+mae(laura, silvia).
+mae(silvia, bruno).
 
-%==== Consultas ====%
-% ?- filho(maria, joao). -> true.
-% ?- irmaos(maria, pedro, joao). -> true.
+% Regras
+avo_paterno(X,Y):- pai(Z,Y),pai(X,Z).
+avo_materno(X,Y):- mae(Z,Y),pai(X,Z).
+
+tem_filho(X):- pai(X,_);mae(X,_).
+
+irmaos(X,Y):- (pai(Z,X),pai(Z,Y));(mae(Z,X),mae(Z,Y)),(X\=Y).
